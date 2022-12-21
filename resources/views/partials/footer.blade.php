f84097ef851cab23af0beab53906cc3eb6962cec
naz1ne <fabio.sirigu.1995@gmail.com>
2022-12-21 16:58:44 +0100

    add db comics

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,30 @@
+<footer id="site_footer">
+      <div class="contact_us">
+            <div class="my_container">
+                  <div class="row">
+                        <div class="col-5">
+                              <div class="row">
+
+                              </div>
+                        </div>
+
+                  </div>
+            </div>
+
+      </div>
+      <div class="call_to_action">
+            <div class="my_container">
+                  <button>
+                        sign-up now!
+                  </button>
+                  <nav class="navbar">
+                        <h3>follow us</h3>
+                        <div>
+
+                        </div>
+                  </nav>
+            </div>
+
+      </div>
+</footer>
+<!-- /#site_footer -->
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header id="site_header">
+      <div class="my_container">
+            <div class="logo">
+                  <img src="" alt="">
+            </div>
+
+            <!-- /.navbar -->
+      </div>
+</header>
