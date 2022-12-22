4b822f6eb2cae27945cac0744eb7b9f5abbcc8bd
naz1ne <fabio.sirigu.1995@gmail.com>
2022-12-22 15:08:52 +0100

    finish ex with bonus all crud and layouts

--- resources/views/partials/comics-modal.blade.php
+++ resources/views/partials/comics-modal.blade.php
@@ -6,7 +6,7 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                   </div>
                   <div class="modal-body">
-                        Stai per effettuare un operazione distruttiva, sei sicuro sicuro?
+                        Stai per cancellare definitivamente un fumetto, sei sicuro?
                   </div>
                   <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
