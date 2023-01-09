c47225401bd63dde98a5dfef239d83a9e9182307
naz1ne <fabio.sirigu.1995@gmail.com>
2023-01-09 16:53:28 +0100

    add style bootstrap invalid

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -10,36 +10,36 @@
 
             <div class="mb-3">
                   <label for="title" class="form-label">Title</label>
-                  <input type="text" name="title" id="title" class="form-control" placeholder="" aria-describedby="titleHlper" value="{{old('title')}}">
+                  <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror" placeholder="" aria-describedby="titleHlper" value="{{old('title')}}">
                   <small id="titleHlper" class="text-muted">Add the comic title here</small>
             </div>
             <div class="mb-3">
                   <label for="thumb" class="form-label">comic Image</label>
-                  <input type="text" name="thumb" id="thumb" class="form-control" placeholder="" aria-describedby="thumbHlper" value="{{old('thumb')}}">
+                  <input type="text" name="thumb" id="thumb" class="form-control @error('thumb') is-invalid @enderror" placeholder="" aria-describedby="thumbHlper" value="{{old('thumb')}}">
                   <small id="thumbHlper" class="text-muted">Add the comic thumb here</small>
             </div>
             <div class="mb-3">
                   <label for="description" class="form-label">Description</label>
-                  <textarea class="form-control" name="description" id="description" rows="4">{{old('description')}}</textarea>
+                  <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description" rows="4">{{old('description')}}</textarea>
             </div>
             <div class="mb-3">
                   <label for="series" class="form-label">Series</label>
-                  <input type="text" name="series" id="series" class="form-control" placeholder="" aria-describedby="seriesHlper" value="{{old('series')}}">
+                  <input type="text" name="series" id="series" class="form-control @error('series') is-invalid @enderror" placeholder="" aria-describedby="seriesHlper" value="{{old('series')}}">
                   <small id="seriesHlper" class="text-muted">Add the comic series here</small>
             </div>
             <div class="mb-3">
                   <label for="price" class="form-label">Price</label>
-                  <input type="text" name="price" id="price" class="form-control" placeholder="" aria-describedby="priceHlper" value="{{old('price')}}">
+                  <input type="text" name="price" id="price" class="form-control @error('price') is-invalid @enderror" placeholder="" aria-describedby="priceHlper" value="{{old('price')}}">
                   <small id="priceHlper" class="text-muted">Add the comic price here</small>
             </div>
             <div class="mb-3">
                   <label for="sale_date" class="form-label">Sale Date</label>
-                  <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="" aria-describedby="sale_dateHlper" value="{{old('sale_date')}}">
+                  <input type="text" name="sale_date" id="sale_date" class="form-control @error('sale_date') is-invalid @enderror" placeholder="" aria-describedby="sale_dateHlper" value="{{old('sale_date')}}">
                   <small id="sale_dateHlper" class="text-muted">Add the comic sale date here</small>
             </div>
             <div class="mb-3">
                   <label for="type" class="form-label">Type</label>
-                  <input type="text" name="type" id="type" class="form-control" placeholder="" aria-describedby="typeHlper" value="{{old('type')}}">
+                  <input type="text" name="type" id="type" class="form-control @error('type') is-invalid @enderror" placeholder="" aria-describedby="typeHlper" value="{{old('type')}}">
                   <small id="typeHlper" class="text-muted">Add the comic type here</small>
             </div>
             <button type="submit" class="btn btn-primary">Submit</button>
